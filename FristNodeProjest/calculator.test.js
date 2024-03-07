0081d0aa147503b2e48f1521d381848fb30a9281
Poobate Nimnuan <111554614+Plashon@users.noreply.github.com>
2024-03-07 16:55:45 +0700

    Delete FristNodeProjest directory

--- FristNodeProjest/calculator.test.js
+++
@@ -1,8 +0,0 @@
-const mathOperation = require("./calculator");
-
-describe("Calculator tets", () => {
-  test("adding 1 + 2 sould return 3", () => {
-    var result = mathOperation.sum(1, 2);
-    expect(result).toBe(3);
-  });
-});
